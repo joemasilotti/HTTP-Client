91c93b10f289cc6f28e27f0736ec1ad4ad49d143
Joe Masilotti <joe@masilotti.com>
2022-11-18 12:29:03 -0700

    Fix exception when running on iOS 14.4

--- Sources/HTTP/RequestLoader.swift
+++ Sources/HTTP/RequestLoader.swift
@@ -4,20 +4,18 @@ public protocol RequestLoader {
     func load(_ request: URLRequest) async throws -> (Data, URLResponse)
 }
 
-@available(macOS 12.0, *)
-@available(iOS 15.0, *)
 extension URLSession: RequestLoader {
     public func load(_ request: URLRequest) async throws -> (Data, URLResponse) {
-        try await data(for: request)
+        if #available(iOS 15.0, *) {
+            return try await data(for: request)
+        } else {
+            return try await _data(for: request)
+        }
     }
-}
 
-@available(macOS, deprecated: 12.0, message: "Use the built-in API instead")
-@available(iOS, deprecated: 15.0, message: "Use the built-in API instead")
-extension URLSession {
-    func data(from url: URL) async throws -> (Data, URLResponse) {
+    private func _data(for request: URLRequest) async throws -> (Data, URLResponse) {
         try await withCheckedThrowingContinuation { continuation in
-            let task = self.dataTask(with: url) { data, response, error in
+            let task = self.dataTask(with: request) { data, response, error in
                 guard let data = data, let response = response else {
                     let error = error ?? URLError(.badServerResponse)
                     return continuation.resume(throwing: error)
